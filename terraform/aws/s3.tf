b48c7e8560a3c14d188f8c4f256054c097edcdd2
Mahesh <manimahesh1@gmail.com>
2021-12-09 11:12:06 -0800

    Bucket is no longer public

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -4,7 +4,6 @@ resource "aws_s3_bucket" "data" {
   # bucket does not have access logs
   # bucket does not have versioning
   bucket        = "${local.resource_prefix.value}-data"
-  acl           = "public-read"
   force_destroy = true
   tags = {
     Name                 = "${local.resource_prefix.value}-data"
@@ -22,6 +21,7 @@ resource "aws_s3_bucket" "data" {
     enabled = "${var.versioning_enabled}"
   }
 }
+
 
 resource "aws_s3_bucket_object" "data_object" {
   bucket = aws_s3_bucket.data.id
