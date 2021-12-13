47ed11a39d7d61196a89060686fce4c9f9b30828
Mahesh <manimahesh1@gmail.com>
2021-12-12 17:54:35 -0800

    Made a change

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -65,9 +65,6 @@ resource "aws_s3_bucket" "operations" {
   # bucket does not have access logs
   bucket = "${local.resource_prefix.value}-operations"
   acl    = "private"
-  versioning {
-    enabled = true
-  }
   force_destroy = true
   tags = {
     Name                 = "${local.resource_prefix.value}-operations"
